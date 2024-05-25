a50b249be96a2c4fa99773e0b702f82a96f60c25
fluke sarun <flukesarans@gmail.com>
2024-05-25 22:27:24 +0800

    Uploaded

---
+++ Date.cpp
@@ -0,0 +1,24 @@
+#include "Date.h"
+
+// Function 1
+double operator-(const Date& d1, const Date& d2)
+{
+  int yearDiff = d1.year - d2.year;
+  int monthDiff = (d1.month - d2.month);
+  int dayDiff = d1.day - d2.day;
+  return yearDiff + monthDiff / 12.0 + dayDiff / 365.0;
+}
+
+// Function 2
+std::ostream& operator<<(std::ostream& os, const Date& d)
+{
+  os << d.year << " " << d.month << " " << d.day << std::endl;
+  return os;
+}
+
+// Function 3
+std::istream& operator>>(std::istream& is, Date& d)
+{
+  is >> d.year >> d.month >> d.day;
+  return is;
+}
