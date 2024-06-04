043e0ad0738c715c6ad9467914b9edac38110aa1
MattisM55 <mattis.m.2023@mqf.smu.edu.sg>
2024-06-04 21:55:26 +0800

    Update Bond.h

--- Bond.h
+++ Bond.h
@@ -1,28 +1,33 @@
 #pragma once
 #include "Trade.h"
-#include "Market.h"
-#include <string>
 
 class Bond : public Trade {
 public:
-    Bond(Date start, Date end, double _notional, double _price, const std::string& _underlying): Trade("BondTrade", start) {
-        startDate = start;
-        maturityDate = end;
+    Bond(Date start, Date end, double _notional, double _price, const string& _underlying): Trade("BondTrade", start) {
         notional = _notional;
         tradePrice = _price;
         underlying = _underlying;
+        startDate = start;
+        maturityDate = end;
     }
 
-    inline double Payoff(double marketPrice) const override{
-        return ((marketPrice  / 100) * notional);
-    } // implement this
+    string getUnderlying() const {return underlying;};
+    double getTradeprice() const {return tradePrice;};
+    double getNotional() const {return notional;};
+    Date getStarting() const {return startDate;};
+    Date getMaturity() const {return maturityDate;};
+    inline double Payoff(double marketPrice) const {
+       return ((marketPrice / 100) * notional);
+    }
+    
+    inline double Payoffswap(double market_rate, const Market&mkt) const {
+        return 0.0;
+    }
 
-    std::string getUnderlying() const { return underlying; }
-
-//private:
+private:
     double notional;
     double tradePrice;
     Date startDate;
     Date maturityDate;
-    std::string underlying;
+    string underlying;
 };
